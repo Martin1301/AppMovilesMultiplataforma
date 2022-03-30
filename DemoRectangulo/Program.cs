01ff0ee1407f9c9207637aabeafb9ae7fe55cc03
Martin1301 <67760752+Martin1301@users.noreply.github.com>
2022-03-30 17:01:57 -0500

    Agregar archivos de proyecto.

---
+++ DemoRectangulo/Program.cs
@@ -0,0 +1,61 @@
+﻿using DemoRectangulo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRectangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo = new Rectangulo();
+            
+            Console.WriteLine("Ingrese las coordenadas del rectangulo");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Ingrese las coordenadas x1 y y1");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            rectangulo.p1 = new Punto(x1, y1);
+            Console.WriteLine("Ingrese las coordenadas x2 y y2");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese las coordenadas x3 y y3");
+            rectangulo.p2 = new Punto(x2, y2);
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese las coordenadas x4 y y4");
+            rectangulo.p3 = new Punto(x3, y3);
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+            rectangulo.p4 = new Punto(x4, y4);
+
+            double base1;
+            double altura1;
+            double base2;
+            double altura2;
+            double res1;
+            double res2;
+            
+            base1 = rectangulo.HallarDistancia1();
+            altura1= rectangulo.HallarDistancia2();          
+            base2 = rectangulo.HallarDistancia3();
+            altura2 = rectangulo.HallarDistancia4();
+            res1 = rectangulo.CalcularArea();
+            res2 = rectangulo.CalcularPerimetro();
+
+            if (base1 == base2 && altura1 == altura2)
+            {
+                Console.WriteLine("El área del rectangulo es: " + res1);
+                Console.WriteLine("El perimetro del rectangulo es: " + res2);
+            }
+            else
+            {
+                Console.WriteLine("Los datos ingresados no son el de un rectangulo");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ DemoRectangulo/Rectangulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRectangulo
+{
+    class Rectangulo
+    {
+        public Punto p1 { get; set; }
+        public Punto p2 { get; set; }
+        public Punto p3 { get; set; }
+        public Punto p4 { get; set; }
+
+        //public double x1 {get; set;}
+        //public double x2 { get; set; }
+        //public double x3 { get; set; }
+        //public double x4 { get; set; }
+        //public double y1 { get; set; }
+        //public double y2 { get; set; }
+        //public double y3 { get; set; }
+        //public double y4 { get; set; }
+
+        public double HallarDistancia1( )
+        {
+            return Math.Sqrt((Math.Pow((p2.x - p1.x), 2) + Math.Pow((p2.y - p1.y), 2)));
+        }
+        public double HallarDistancia2()
+        {
+            return Math.Sqrt((Math.Pow((p3.x - p2.x), 2) + Math.Pow((p3.y - p2.y), 2)));
+        }
+        public double HallarDistancia3()
+        {
+            return Math.Sqrt((Math.Pow((p4.x - p3.x), 2) + Math.Pow((p4.y - p3.y), 2)));
+        }
+        public double HallarDistancia4()
+        {
+            return Math.Sqrt((Math.Pow((p1.x - p4.x), 2) + Math.Pow((p1.y - p4.y), 2)));
+        }
+        public double CalcularArea()
+        {
+            return HallarDistancia1() * HallarDistancia2();
+        }
+        public double CalcularPerimetro()
+        {
+            return 2*(HallarDistancia1() + HallarDistancia2()); 
+        }
+    }
+}
